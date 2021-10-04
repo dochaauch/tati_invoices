8e60806995385b40ca88994f2a12b50c3bdf030b
dochaauch <lena.docha@gmail.com>
2021-10-04 21:35:09 +0200

    palk udpate target folder

--- palk_pank.py
+++ palk_pank.py
@@ -4,6 +4,7 @@ import pandas as pd
 from textwrap import wrap
 import re
 from sys import argv
+import conf
 
 def check_if_not_nan(str_):
     if not pd.isna(str_):
@@ -28,7 +29,8 @@ def del_empty_line(str_):
             return s
 
 
-your_target_folder = rf'C:\Users\docha\OneDrive\Leka'
+#your_target_folder = rf'C:\Users\docha\OneDrive\Leka'
+your_target_folder = conf.your_target_folder
 my_file = rf'{your_target_folder}\Union Salary\salary_template.xlsx'
 my_check = rf'{your_target_folder}\Union Salary\konto_check.xlsx'
 company_name = 'UNIONCOMPANY OÜ'
